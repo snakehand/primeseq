e0c942f328e02bd5d9f2d1d12d5d971e0b4d2cdb
Frank A. Stevenson <frank.stevenson@magseis.com>
2021-02-27 14:39:15 +0100

    Remove multiples of 3 from testing

--- src/main.rs
+++ src/main.rs
@@ -10,23 +10,31 @@ fn test(start: u32, num: u32) -> bool {
     for i in start..=num {
         number.push_str(&format!("{}",i));
     }
+    let csum: u32 = number.chars().map(|c| c.to_digit(10).unwrap()).sum();
+    if csum % 3 == 0 {
+        // Multiple of 3
+        return false;
+    }
     let prime = is_prime(&number);
     if prime {
         println!("Prime: {}->{}", start, num);
     } else {
-        // println!("[{}]", num);
+        println!("[{}]", num);
     }
     prime
 }
 
 
 fn check_start(start: u32) {
-    let pr = ((start+1)..1000_u32).into_par_iter().any(|n| test(start,n));
+    let pr = ((start+1)..10000_u32).into_par_iter().any(|n| test(start,n));
     println!("pr {} {}", start, pr);
 }
 
 fn main() {
+    /*
     for num in 20..30 {
         check_start(num);
     }
+    */
+    check_start(22);
 }
